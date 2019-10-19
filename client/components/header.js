8c7aaf4378b4fd974e548a8f25bf036b79bab0c9
issa ouedraogo <i.ouedraogo49@yahoo.fr>
2019-10-19 17:29:58 -0400

    messenger widget

--- client/components/header.js
+++ client/components/header.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+  } from 'reactstrap';
 class NavigationMenu extends Component {
-
     render() {
       return (
-        <div className="container">
-          // this is the header container
-        </div>
+      <div>    
+        <Navbar color="dark" dark expand="lg" >
+          <NavbarBrand href="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AI-CAREER-COACH</NavbarBrand>
+          <NavbarToggler />
+          <Collapse navbar>
+            <Nav className="ml-auto" Navbar>
+              <NavItem>
+                <NavLink href=''></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#"></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="#"></NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+       </div>
       );
     }
   }
--- client/store/index.js
+++ client/store/index.js
@@ -3,7 +3,6 @@ import { logger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import reducer from './reducer';
 import 'bootstrap/dist/css/bootstrap.css';
-
 const store = createStore(reducer, applyMiddleware(logger, thunkMiddleware));
 
 export default store;
