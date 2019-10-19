9e2f4c3389896a941b19cd7f8731dce5c3c9501b
issa ouedraogo <i.ouedraogo49@yahoo.fr>
2019-10-19 10:41:36 -0400

    bootstrap incorparated

--- client/store/index.js
+++ client/store/index.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import reducer from './reducer';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const store = createStore(reducer, applyMiddleware(logger, thunkMiddleware));
 
