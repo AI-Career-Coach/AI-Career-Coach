0deac80bcf28cb6342f93b1da95c2879aa239d37
tjhelsel <terence.helsel@gmail.com>
2019-10-19 14:16:28 -0400

    fix: package-lock fixed, incorrect bootstrap import removed

--- client/store/index.js
+++ client/store/index.js
@@ -2,7 +2,6 @@ import { createStore, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import reducer from './reducer';
-import 'bootstrap/dist/css/bootstrap.css';
 
 const store = createStore(reducer, applyMiddleware(logger, thunkMiddleware));
 
